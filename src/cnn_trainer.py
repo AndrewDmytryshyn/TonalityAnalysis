263922bf63751e1d97620d263e08844c91b1c54f
AndrewDmytryshyn <61089704+AndrewDmytryshyn@users.noreply.github.com>
2021-05-22 23:19:24 +0300

    Add files via upload

---
+++ src/cnn_trainer.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+
+# розпаковуємо розмічені датасети
+n = ['id', 'date', 'name', 'text', 'typr', 'rep', 'rtw', 'faw', 'stcount', 'foll', 'frien', 'listcount']
+data_positive = pd.read_csv('data/positive.csv', sep=';', error_bad_lines=False, names=n, usecols=['text'])
+data_negative = pd.read_csv('data/negative.csv', sep=';', error_bad_lines=False, names=n, usecols=['text'])
+
+sample_size = min(data_positive.shape[0], data_negative.shape[0])
+raw_data = np.concatenate((data_positive['text'].values[:sample_size],
+                           data_negative['text'].values[:sample_size]), axis=0)
+labels = [1] * sample_size + [0] * sample_size
+
+import re
+
+
+# функція для обробки тексту
+def preprocess_text(text):
+    text = text.lower().replace("ё", "е")
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text)
+    text = re.sub('@[^\s]+', 'USER', text)
+    text = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', text)
+    text = re.sub(' +', ' ', text)
+    return text.strip()
+
+
+# обробляєм текст
+data = [preprocess_text(t) for t in raw_data]
+
+from sklearn.model_selection import train_test_split
+
+# розбиваєм набір даних на тренувальну і навчальні вибірки
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=2)
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+# відображення кожного речення в масив ідентифікаторів токенів
+# максимальна кількість слів в реченні
+SENTENCE_LENGTH = 26
+# розмір словника
+NUM = 100000
+
+
+def get_sequences(tokenizer, x):
+    sequences = tokenizer.texts_to_sequences(x)
+    return pad_sequences(sequences, maxlen=SENTENCE_LENGTH)
+
+
+tokenizer = Tokenizer(num_words=NUM)
+tokenizer.fit_on_texts(x_train)
+
+x_train_seq = get_sequences(tokenizer, x_train)
+x_test_seq = get_sequences(tokenizer, x_test)
+
+# створення і налаштування нейронної мережі
+from gensim.models import Word2Vec
+
+# загружаем навчену модель векторів слів
+w2v_model = Word2Vec.load('models/w2v/tweets_model.w2v')
+DIM = w2v_model.vector_size
+# заповняєм embedding шар нулями
+embedding_matrix = np.zeros((NUM, DIM))
+# додаємо NUM=100000 слів які зустрічалися найбільше з навчальної вибірки в embedding шар
+for word, i in tokenizer.word_index.items():
+    if i >= NUM:
+        break
+    if word in w2v_model.wv.vocab.keys():
+        embedding_matrix[i] = w2v_model.wv[word]
+
+from keras.layers import Input
+from keras.layers.embeddings import Embedding
+
+tweet_input = Input(shape=(SENTENCE_LENGTH,), dtype='int32')
+tweet_encoder = Embedding(NUM, DIM, input_length=SENTENCE_LENGTH,
+                          weights=[embedding_matrix], trainable=False)(tweet_input)
+
+from keras import optimizers
+from keras.layers import Dense, concatenate, Activation, Dropout
+from keras.models import Model
+from keras.layers.convolutional import Conv1D
+from keras.layers.pooling import GlobalMaxPooling1D
+
+branches = []
+x = Dropout(0.2)(tweet_encoder)
+
+for size, filters_count in [(2, 10), (3, 10), (4, 10), (5, 10)]:
+    for i in range(filters_count):
+        branch = Conv1D(filters=1, kernel_size=size, padding='valid', activation='relu')(x)
+        branch = GlobalMaxPooling1D()(branch)
+        branches.append(branch)
+
+x = concatenate(branches, axis=1)
+x = Dropout(0.2)(x)
+x = Dense(30, activation='relu')(x)
+x = Dense(1)(x)
+output = Activation('sigmoid')(x)
+
+# навчаємо нейронну мережу
+model = Model(inputs=[tweet_input], outputs=[output])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[precision, recall, f1])
+model.summary()
+
+#зберігаєм навчену модель
+model.save_weights('cnn/cnn-trainable-03-0.78.hdf5')
