3004492df888a58289752e459f461d951a0124c0
vitaliycherednik <70630561+vitaliycherednik@users.noreply.github.com>
2021-05-22 23:57:53 +0300

    Add files via upload

---
+++ src/tonality.py
@@ -0,0 +1,22 @@
+from keras.models import Model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+def get_sequences(tokenizer, x):
+    sequences = tokenizer.texts_to_sequences(x)
+    return pad_sequences(sequences, maxlen=26)
+
+
+def tonality(str):
+    model = Model()
+    model.load_weights('cnn/cnn-trainable.hdf5')
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts([str])
+    x_test_seq1 = get_sequences(tokenizer, [str])
+    predicted = model.predict(x_test_seq1)
+    if predicted < 0.33:
+        print("Negative, " + predicted)
+    elif predicted < 0.66:
+        print("Neutral, " + predicted)
+    else:
+        print("Positive, " + predicted)
